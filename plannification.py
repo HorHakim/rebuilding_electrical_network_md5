dde09c1741a26a5b0ad18fed63b41ffbdf60d1f0
Hakim Horairy <horairyhakim@gmail.com>
2024-05-14 11:25:53 +0200

    init project skelton

---
+++ plannification.py
@@ -0,0 +1,14 @@
+class Planification:
+	def __init__(self, csv_path):
+		pass
+
+	def prepare_data(self):
+		pass
+
+	def run_planification_algo(self):
+		pass
+
+if __name__ == "__main__":
+	planification_object = Planification()
+	planification_object.prepare_data()
+	planification_object.run_planification_algo()
